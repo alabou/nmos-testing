49cf76f00cb6655107cfe2c8998aaebc2025fc3b
Alain Bouchard <abouchar@matrox.com>
2025-11-16 09:49:35 -0500

    update

--- start_capture_pcap.bat
+++ start_capture_pcap.bat
@@ -9,11 +9,11 @@ if "%IPMX_VENDOR_PCAP_CAPTURE%" == "LOCAL" (
 if "%IPMX_VENDOR_PCAP_CAPTURE%" == "VB440" (
     @REM for /f "usebackq delims=" %%i in (`ssh capture@10.20.10.194 "capture/capture.mjs %2 %3 %4 | tail -n1"`) do curl -LRs %%i -o %1
     if "%4" == "video" (
-        powershell -Command "curl.exe -k 'https://192.168.112.57/probe/api/captures/?responseMode=pcap&deleteOnComplete' --json '{\"receiverIds\":[\"e64f24f4-e584-5d4a-9d26-0507dde17e65\"],\"packetLimit\":3000,\"timeLimit\":5000,\"captureRTCP\":true}' -o %1"    
+        powershell -Command "curl.exe -k 'https://192.168.112.57/probe/api/captures/?responseMode=pcap&deleteOnComplete' --json '{""receiverIds"":[""e64f24f4-e584-5d4a-9d26-0507dde17e65""],""packetLimit"":3000,""timeLimit"":5000,""captureRTCP"":true}' -o %1"    
         goto :vb440_done
     )
     if "%4" == "audio" (
-        powershell -Command "curl.exe -k 'https://192.168.112.57/probe/api/captures/?responseMode=pcap&deleteOnComplete' --json '{\"receiverIds\":[\"ce278320-000b-102b-aa03-000000000000\"],\"packetLimit\":3000,\"timeLimit\":5000,\"captureRTCP\":true}' -o %1"    
+        powershell -Command "curl.exe -k 'https://192.168.112.57/probe/api/captures/?responseMode=pcap&deleteOnComplete' --json '{""receiverIds"":[""ce278320-000b-102b-aa03-000000000000""],""packetLimit"":3000,""timeLimit"":5000,""captureRTCP"":true}' -o %1"    
         goto :vb440_done
     )
     echo Invalid format: %4
